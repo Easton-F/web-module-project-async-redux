e50d5c429e4de5565fe14bb2698713bf3628f81c
Easton <easton.fritsch21@gmail.com>
2023-08-01 16:14:16 -0600

    set up useEffect for fetchStart

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import GifList from './Components/GifList';
 import GifForm from './Components/GifForm';
 import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchStart } from './actions/index';
 
 function App(props) {
-  const { loading, error } = props;
+  const { loading, error, fetchStart } = props;
+
+  useEffect(() => {
+    fetchStart();
+  }, []);
 
   return (
     <div className="App">
@@ -32,4 +38,5 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+
+export default connect(mapStateToProps, { fetchStart })(App);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,13 +1,20 @@
 import data from './../data/gifs';
+import { FETCH_START } from '../actions';
 
 const initialState = {
-    gifs: data,
+    gifs: [],
     loading: false,
     error: ""
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCH_START:
+            return {
+                ...state,
+                loading: true,
+                error: ""
+            }
         default:
             return(state);
     }
